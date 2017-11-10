e1cc8dc5da4bc74826a73f7821470799d2f2759c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-11-10 01:02:36 -0800

    Fixing the overloading issues

--- lib/jsonapi/resources/home.rb
+++ lib/jsonapi/resources/home.rb
@@ -1,9 +1,18 @@
 require "jsonapi-resources"
 
 module JSONAPI
+  module ActsAsResourceController
+    module ClassMethods
+      def home_discoverable(description: nil, location: nil)
+        instance_variable_set(:@jsonapi_resources_home, true)
+        instance_variable_set(:@jsonapi_resources_home_description, description)
+        instance_variable_set(:@jsonapi_resources_home_location, location)
+      end
+    end
+  end
+
   module Resources
     module Home
-      require_relative "../acts_as_resource_controller"
       require_relative "home/inflector"
       require_relative "home/engine"
     end
